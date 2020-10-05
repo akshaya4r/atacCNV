981ef66984d5f57200a07ae96bd4253a893b5ed5
akshaya4r <akshaya4r@gmail.com>
2020-10-05 12:42:33 +0200

    Re-organize functions in files

--- R/generateCountMatrix.R
+++ R/generateCountMatrix.R
@@ -40,3 +40,14 @@ generateCountMatrix <- function(windows, bamfiles, remove = c("chrM","chrX","chr
 
   return(se)
 }
+
+
+countInsertions <- function(windows, bamfiles, by = "RG"){
+  # seqlevelsStyle(windows) <- 'NCBI'
+  counts_bam <- summarizeOverlaps(windows, bamfiles, singleEnd=TRUE, fragments=FALSE, mode='Union', param = ScanBamParam(mapqFilter=10), ignore.strand = TRUE)
+  sparseM <- Matrix(assays(counts_bam)$counts, sparse = TRUE)
+  frip <- 1
+  total <- colSums(sparseM)
+  out <- list(counts = sparseM, frip = frip, total = total)
+  return(out)
+}
