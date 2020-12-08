f033b574ba9a3d76cc033e53a8b3c0793c651d31
akshaya4r <akshaya4r@gmail.com>
2020-12-08 13:00:13 +0100

    debugging t-test

--- R/atacCNV.R
+++ R/atacCNV.R
@@ -87,6 +87,7 @@ atacCNV <- function(input, outdir, blacklist, windowSize, reuse.existing=FALSE){
       print("Calculating distance AD")
       results <- lapply(peaksperchrom, function(x2) {
         # x2 <- runmed(x2, k=11, endrule = 'median')
+        print("Running chromosome")
         getbp(x2, k = k, minsize = minsize, test='AD', pcutoff=0.000001)
       })
       # clusterperchrom <- lapply(results, '[[', 3)
